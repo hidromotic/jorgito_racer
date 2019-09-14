bf28a8d021c9c6ac5082e5f748086f0f41c78667
alejosgi <alejosgi@gmail.com>
2019-09-13 22:03:20 -0300

    Update PIDNuestro1.ino

--- PIDNuestro1/PIDNuestro1.ino
+++ PIDNuestro1/PIDNuestro1.ino
@@ -17,9 +17,14 @@ int pulsador = 4;
 
 float kP = 0.015; float kI = 0.0003; float kD = 0.2; // PID
 #define PULSADOR_PRESIONADO (digitalRead(pulsador) == HIGH)
+//http://blog.theinventorhouse.org/usando-las-entradas-analogicas-de-arduino-como-digitales/
+//A0 a A5 --> 14 a 19
 
-#define PIN_LED_TEST A5
+//LED DE TESTEO
+#define PIN_LED_TEST 13
 #define CONFIGURAR_LED_TEST pinMode(PIN_LED_TEST, OUTPUT)
+#define ENCENDER_LED_TEST digitalWrite(PIN_LED_TEST, HIGH)
+#define APAGAR_LED_TEST digitalWrite(PIN_LED_TEST, LOW)
 
 
 void setup() {
@@ -27,10 +32,48 @@ void setup() {
 	Serial.begin(9600);
 	motorI.setSpeed(50); motorD.setSpeed(50); motorI.run(FORWARD); motorD.run(FORWARD); //(FORWARD = ADELANTE) – (BACKWARD = ATRAS) – (RELEASE = DETENER)	
 	pinMode(pulsador, INPUT);
-//	CONFIGURAR_LED_TEST;
-}
+	CONFIGURAR_LED_TEST;
+	APAGAR_LED_TEST;
+	motorI.setSpeed(0); motorD.setSpeed(0);
+	}
+
+void LedTest(void)
+	{
+	static unsigned long ms_ant=0;
+	static bool estado=0;
+	if(millis()-ms_ant < 500) return;
+	ms_ant=millis();
+	estado=!estado;
+	if(estado) ENCENDER_LED_TEST;
+	else 		APAGAR_LED_TEST;
+	}
+/*
+Utilizar el botón para cambiar de estado
+Botón presionado por más de 2 segundos --> arranca al liberar ---> Destellar rápido indicando que está listo para arrancar
+Un toque del botón --> Detiene el motor
+*/
+#define MODO_DETENIDO	0
+#define MODO_LANZADOR	1
+#define MODO_CARRERA	2
+
+unsigned modo=MODO_DETENIDO;
+
+void SupervisaBoton(void)
+	{
+	static bool boton_presionado_ant=0;
+	static unsigned long ms_ant=0;
+	//Si está en CARRERA y se presiona el botón --> pasar a modo DETENIDO
+	//Si está en LANZADOR y se libera el botón --> pasar a modo CARRERA
+	if(PULSADOR_PRESIONADO && !boton_presionado_ant)	//Se presionó el botón
+		{
+		if(modo==MODO_CARRERA) {MODO_DETENIDO; return;}
+		}
+	}
 
 void loop() {
+	LedTest();
+	// SupervisaBoton();
+	// if(estado!=STA_CARRERA) return;	//Queda a la espera del modo carrera
 	if(PULSADOR_PRESIONADO){
 		motorI.run(RELEASE); motorD.run(RELEASE);
 		Serial.println("Apagado");
@@ -85,8 +128,8 @@ void loop() {
 
 
   	errAnt = err;
-	if(millis() - tiempo > 100)
-		tiempo = millis() - tiempo;
+	// if(millis() - tiempo > 100)
+	// 	tiempo = millis() - tiempo;
 
 	//DEBUG
 	return;
